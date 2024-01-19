dc8b4753e502fb0ecbdae8c2d1a9af07c4d460ee
Stefan Jaroschek <stefan@jaroschek.org>
2024-01-19 09:51:33 +0100

    Migrate config entry to add unique id and product name from device

--- custom_components/eaton_ups/__init__.py
+++ custom_components/eaton_ups/__init__.py
@@ -1,12 +1,22 @@
 """The Eaton UPS integration."""
 from __future__ import annotations
 
+import logging
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 
-from .const import DOMAIN, PLATFORMS
+from .api import SnmpApi
+from .const import (
+    DOMAIN,
+    PLATFORMS,
+    SNMP_OID_IDENT_PRODUCT_NAME,
+    SNMP_OID_IDENT_SERIAL_NUMBER,
+)
 from .coordinator import SnmpCoordinator
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
@@ -34,3 +44,24 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
+
+
+async def async_migrate_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Migrate old entry."""
+    _LOGGER.debug("Migrating from version %s", entry.version)
+
+    if entry.version == 1:
+        api = SnmpApi(entry.data)
+        result = api.get([SNMP_OID_IDENT_PRODUCT_NAME, SNMP_OID_IDENT_SERIAL_NUMBER])
+
+        hass.config_entries.async_update_entry(
+            entry,
+            unique_id=result.get(SNMP_OID_IDENT_SERIAL_NUMBER),
+            title=result.get(SNMP_OID_IDENT_PRODUCT_NAME),
+        )
+
+        entry.version = 2
+
+    _LOGGER.info("Migration to version %s successful", entry.version)
+
+    return True
