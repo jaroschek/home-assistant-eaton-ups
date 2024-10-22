c19b1e17d3abdf2e7283a34047630e00edea69fd
Stefan Jaroschek <stefan@jaroschek.org>
2024-10-22 21:06:40 +0200

    Add alternate XUPS OIDs for firmware, product name and serial number

--- custom_components/eaton_ups/entity.py
+++ custom_components/eaton_ups/entity.py
@@ -11,9 +11,12 @@ from .const import (
     MANUFACTURER,
     SNMP_OID_BATTERY_CAPACITY,
     SNMP_OID_IDENT_FIRMWARE_VERSION,
+    SNMP_OID_IDENT_FIRMWARE_VERSION_XUPS,
     SNMP_OID_IDENT_PART_NUMBER,
     SNMP_OID_IDENT_PRODUCT_NAME,
+    SNMP_OID_IDENT_PRODUCT_NAME_XUPS,
     SNMP_OID_IDENT_SERIAL_NUMBER,
+    SNMP_OID_IDENT_SERIAL_NUMBER_XUPS,
 )
 from .coordinator import SnmpCoordinator
 
@@ -50,8 +53,14 @@ class SnmpEntity(CoordinatorEntity[SnmpCoordinator]):
         return self.coordinator.data.get(
             SNMP_OID_IDENT_SERIAL_NUMBER,
             self.coordinator.data.get(
-                SNMP_OID_IDENT_PART_NUMBER,
-                self.coordinator.data.get(SNMP_OID_IDENT_PRODUCT_NAME),
+                SNMP_OID_IDENT_SERIAL_NUMBER_XUPS,
+                self.coordinator.data.get(
+                    SNMP_OID_IDENT_PART_NUMBER,
+                    self.coordinator.data.get(
+                        SNMP_OID_IDENT_PRODUCT_NAME,
+                        self.coordinator.data.get(SNMP_OID_IDENT_PRODUCT_NAME_XUPS),
+                    ),
+                ),
             ),
         )
 
@@ -62,9 +71,18 @@ class SnmpEntity(CoordinatorEntity[SnmpCoordinator]):
             identifiers={(DOMAIN, self.identifier)},
             manufacturer=MANUFACTURER,
             model=self.coordinator.data.get(SNMP_OID_IDENT_PART_NUMBER),
-            name=self.coordinator.data.get(SNMP_OID_IDENT_PRODUCT_NAME),
-            serial_number=self.coordinator.data.get(SNMP_OID_IDENT_SERIAL_NUMBER),
-            sw_version=self.coordinator.data.get(SNMP_OID_IDENT_FIRMWARE_VERSION),
+            name=self.coordinator.data.get(
+                SNMP_OID_IDENT_PRODUCT_NAME,
+                self.coordinator.data.get(SNMP_OID_IDENT_PRODUCT_NAME_XUPS),
+            ),
+            serial_number=self.coordinator.data.get(
+                SNMP_OID_IDENT_SERIAL_NUMBER,
+                self.coordinator.data.get(SNMP_OID_IDENT_SERIAL_NUMBER_XUPS),
+            ),
+            sw_version=self.coordinator.data.get(
+                SNMP_OID_IDENT_FIRMWARE_VERSION,
+                self.coordinator.data.get(SNMP_OID_IDENT_FIRMWARE_VERSION_XUPS),
+            ),
         )
 
     @property
